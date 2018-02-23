75333ef291c40e64cf5cf6a63acb4dc237bac073
praveenpravin111 <36357471+praveenpravin111@users.noreply.github.com>
2018-02-23 17:59:02 +0530

    odd-even.cpp

---
+++ odd-even.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+signed long number;
+cout<<"\nenter the number";
+cin>>number;
+if(number%2==0)
+{
+cout<<"\n number is even";
+}
+else
+{
+cout<<"\n number is odd";
+}
+return 0;
+}
